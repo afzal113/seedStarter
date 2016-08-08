cc9fb6f077b1ada9a44c59968b73d07e90214ceb
afzal agwan <afzalagwan113113@gmail.com>
2016-08-08 12:12:29 -0400

    update with karama coverage

---
+++ src/babysit.js
@@ -0,0 +1,7 @@
+var babysitterSalary = function(hour) {
+    var convertedSalary = '';
+    while (hour > 0) {
+        hour -= 1;
+    }
+
+}
---
+++ test/babysit.spec.js
@@ -0,0 +1,5 @@
+describe("babysitter salary", function() {
+    it('description', function() {
+
+    });
+});
